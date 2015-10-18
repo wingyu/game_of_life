2f0d1116f6d3056aa9ace24135ac5e514a362402
Vincent Wong <wingyu64@gmail.com>
2015-10-18 20:24:52 +1100

    Finished basic acceptance spec

--- spec/integration/running_a_game.rb
+++ spec/integration/running_a_game.rb
@@ -2,14 +2,13 @@ require "spec_helper"
 
 module GameOfLife
   describe "Runs the Game of Life with applied rules" do
+    let(:game) { GameOfLife::Game.new(turns: 1, grid_size:3) }
 
+    it "only has 0 cells left" do
+      game.seed_life([1,1])
+
+      expect(game.grid.live_cell_count).to eq 0
+    end
   end
-
 end
 
-=begin
-game = Game.new(turns: 1, size: 3)
-  describe "Applies the rule's of Game of Life" do
-    game.board.live_cell_count = Y
-  end
-=end
