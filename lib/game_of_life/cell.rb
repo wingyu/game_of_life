795fa29aa4df05d0afbabf599d8f4a85b4fbe35d
Vincent Wong <wingyu64@gmail.com>
2015-10-19 12:38:17 +1100

    Basic cell classic

---
+++ lib/game_of_life/cell.rb
@@ -0,0 +1,12 @@
+module GameOfLife
+  class Cell
+    attr_accessor :x, :y, :live_neighbours, :alive
+
+    def initialize(x, y)
+      @x = x
+      @y = y
+      @live_neighbours = 0
+      @alive = false
+    end
+  end
+end
