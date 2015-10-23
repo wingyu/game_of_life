ed23665e0778840bbe4be650ece8b1234bc3986e
Vincent <vincent@Vincents-MBP.unknown>
2015-10-23 15:44:17 +1100

    Added live,die and react methods to Cell class

--- lib/game_of_life/cell.rb
+++ lib/game_of_life/cell.rb
@@ -1,6 +1,6 @@
 module GameOfLife
   class Cell
-    attr_accessor :x, :y, :live_neighbours, :alive
+    attr_accessor :x, :y, :live_neighbours
 
     def initialize(x:, y:)
       @x = x
@@ -8,5 +8,39 @@ module GameOfLife
       @live_neighbours = 0
       @alive = false
     end
+
+    def alive?; @alive; end
+
+    def live!
+      @alive = true
+    end
+
+    def react
+      if good_conditions?
+        live!
+      elsif bad_conditions?
+        die!
+      end
+
+      @live_neighbours = 0
+    end
+
+    def increment_live_neighbours
+      @live_neighbours += 1
+    end
+
+    private
+
+      def die!
+        @alive = false
+      end
+
+      def good_conditions?
+        live_neighbours == 3
+      end
+
+      def bad_conditions?
+        live_neighbours > 3 || live_neighbours < 2
+      end
   end
 end
--- spec/unit/cell_spec.rb
+++ spec/unit/cell_spec.rb
@@ -1,7 +1,46 @@
 require "spec_helper"
 
-describe Cell do
+module GameOfLife
+  describe Cell do
+    let(:cell) { Cell.new(x: 1, y: 1) }
 
+    describe "#live!" do
+      it "should set cell's alive status to true" do
+        expect{ cell.live! }.to change{ cell.alive? }.
+          from(false).to(true)
+      end
+    end
 
+    describe "#react" do
+      context "cell is alive" do
+        before { cell.live! }
 
+        context "if it has greater than 3 live neighbours" do
+          it "will die" do
+            cell.live_neighbours = 4
+            expect{ cell.react }.to change{ cell.alive? }.
+              from(true).to(false)
+          end
+        end
+
+        context "if it has less than 2 live neighbours" do
+          it "will die" do
+            cell.live_neighbours = 1
+            expect{ cell.react }.to change{ cell.alive? }.
+              from(true).to(false)
+          end
+        end
+      end
+
+      context "cell is dead" do
+        context "if it has exactly 3 live neighbours" do
+          it "will come to life" do
+            cell.live_neighbours = 3
+            expect{ cell.react }.to change{ cell.alive? }.
+              from(false).to(true)
+          end
+        end
+      end
+    end
+  end
 end
